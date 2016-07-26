0d1a98252208030c275e1013dbe43136f62b976c
Paul Biggar <paul.biggar@gmail.com>
2016-07-26 12:53:42 -0700

    Get test reporting vaguely working for the first time

--- test/cljs/nativeparedit/test_core.cljs
+++ test/cljs/nativeparedit/test_core.cljs
@@ -13,7 +13,7 @@
 (deftest errors-testing
   (is
    (throw (js/Error. "bug!"))))
-
+;
 
 
 
@@ -42,6 +42,7 @@
 
 
 
+
 ;;;;;;;;;;;;;;;;;;;;
 ;;; test setup
 ;;;;;;;;;;;;;;;;;;;;
@@ -54,43 +55,71 @@
 ;; todo store it in the test env
 (def results (atom (list)))
 (def specs (atom (list)))
-(def suite (atom nil))
 
-;describe("asdas", -> it("asdasdasd", -> expect(1).toEqual(1)))
-  ; (let [actual (:actual m)
-  ;       expected (:expected m)
-  ;       result (-> expected js/expect (.toEqual actual))])
 
-(defn add-result [type result]
-  (println type)
-  (println result))
+(defn clear-test-data! []
+  (swap! results (fn [_] (list))))
 
-(defn add-spec [spec]
-  (let [spec (js/it (-> m :name str) #(add-results % @results))]
-    (swap! conj specs spec))
-  (println val))
+(defn clear-suite-data! []
+  (swap! specs (fn [_] (list))))
 
-(defn add-suite [suite]
-  ()
-  (let [s (js/describe (-> :ns m str) (fn [] nil))]
-    (js/console.log s)
-    (reset! suite s))
-  )
+(defn add-result! [type result]
+  (swap! results conj {:type type :result result}))
+
+(defn add-spec! [spec]
+  (swap! specs conj {:spec spec :results @results}))
+
+
+
+;; creating
+(defn create-results [results]
+  (println "creating results")
+  (doall (for [{:keys [result type]} results]
+           (do
+            (println "result: ")
+            (println result)
+            (println type)
+            (condp = type
+                   :error (let [spec (-> js/jasmine .getEnv .-currentSpec) ]
+                            (.fail spec {:message "asdas" :stack []}))
+                   :pass []
+                   :fail (let [actual (str (:actual result))
+                               expected (str (:expected result))]
+                           (-> actual js/expect (.toEqual expected))))))))
+
+(defn create-all-specs [spec]
+  (doseq [s @specs]
+    (println "creating spec")
+    (js/it (-> s :var str) #(create-results (:results s)))))
+
+(defn create-suite! [suite]
+  (js/describe (-> :ns suite str) create-all-specs))
+
+
 
 (defmethod cljs.test/report [:cljs.test/default :fail] [m]
-  (add-result :fail m))
+  (println "fail")
+  (add-result! :fail m))
 
 (defmethod cljs.test/report [:cljs.test/default :pass] [m]
-  (add-result :pass m))
+  (println "pass")
+  (add-result! :pass m))
 
 (defmethod cljs.test/report [:cljs.test/default :error] [m]
-  (add-result :error m))
-
-(defmethod cljs.test/report [:cljs.test/default :end-test-ns] [m]
-  (add-suite m))
+  (println "error")
+  (add-result! :error m))
 
 (defmethod cljs.test/report [:cljs.test/default :end-test-var] [m]
-  (add-spec m))
+  (println (str "adding spec" m))
+  (add-spec! (:var m))
+  (println "clearing test data")
+  (clear-test-data!))
+
+(defmethod cljs.test/report [:cljs.test/default :end-test-ns] [m]
+  (println "creating suite")
+  (create-suite! m)
+  (println "clearing suite data")
+  (clear-suite-data!))
 
 
 ; (defmethod cljs.test/report [:cljs.test/default :fail] [m]

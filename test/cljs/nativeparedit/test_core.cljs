876355f0974bc12d59aa6ffa198bbe2cd1d8a21a
Paul Biggar <paul.biggar@gmail.com>
2016-07-26 17:28:27 -0700

    Move atom-reporter to its own namespace

--- test/cljs/nativeparedit/test_core.cljs
+++ test/cljs/nativeparedit/test_core.cljs
@@ -1,5 +1,6 @@
 (ns nativeparedit.test-core
-  (:require [cljs.test :refer-macros [deftest is testing run-tests] :as test]
+  (:require [cljs.test :refer-macros [deftest is testing] :as test]
+            [atom-reporter]
             [nativeparedit.core :as np]))
 
 (set! js/saved_clj (clj->js []))
@@ -48,96 +49,5 @@
              (is (= expected-col (-> ed .getCursorBufferPosition .col))))))
 
 
-
-;;;;;;;;;;;;;;;;;;;;
-;;; test setup
-;;;;;;;;;;;;;;;;;;;;
-
-;; todo store it in the test env
-(def results (atom (list)))
-(def specs (atom (list)))
-
-
-(defn clear-results! []
-  (swap! results (fn [_] (list))))
-
-(defn clear-specs! []
-  (swap! specs (fn [_] (list))))
-
-(defn add-result! [result]
-  (swap! results conj
-         (js/jasmine.ExpectationResult. (clj->js result))))
-
-(defn add-spec! [spec]
-  (swap! specs conj {:spec spec :results @results})
-  (clear-results!))
-
-
-;;; Take cljs.test structures and get them into jasmine.
-;;; Creates a suite (using `describe`) for each namespace,
-;;; and a spec (using `it`) for each test var.
-;;;
-;;; TODO: namespaces can nest, so we can handle `testing` this way too.
-;;;
-;; You would think that we could just create the data structures in some form
-;;; jasmine expects and pass them to it. Actually, the jasmine code is super
-;;; convaluated, and you end up just reimplementing it. Whereas calling `describe`
-;;; and `it` directly works quite well.
-
-
-(defn create-results [results]
-  (let [spec (-> js/jasmine .getEnv .-currentSpec)]
-    (doseq [result results]
-      (-> spec .-results_ (.addResult result)))))
-
-(defn create-all-specs []
-  (doseq [s @specs]
-    (js/it (->> s :spec meta :name (str " should pass: ")) #(create-results (:results s)))))
-
-(defn create-suite! [suite]
-  (js/describe (-> :ns suite str) create-all-specs)
-  (clear-specs!))
-
-
-(defmethod cljs.test/report [:cljs.test/default :fail] [m]
-  (add-result! {:passed false
-                :line (:line m)
-                :expected (-> m :expected str)
-                :actual (-> m :actual str)
-                :trace {:stack nil}
-                :message (str "Expected " (:expected m) ", but got " (:actual m))}))
-
-(defmethod cljs.test/report [:cljs.test/default :pass] [m]
-  (add-result! {:passed true
-                :expected (-> m :expected str)
-                :actual (-> m :actual str)
-                :trace {:stack nil}
-                :message "Passed"}))
-
-(defmethod cljs.test/report [:cljs.test/default :error] [m]
-  (add-result! {:passed false
-                :fileName (:file m)
-                :message (-> m :actual .-message)
-                :trace {:stack (-> m :actual .-stack)}}))
-
-(defmethod cljs.test/report [:cljs.test/default :end-test-var] [m]
-  (add-spec! (:var m)))
-
-(defmethod cljs.test/report [:cljs.test/default :end-test-ns] [m]
-  (create-suite! m))
-
-
-; (defmethod cljs.test/report [:cljs.test/default :fail] [m]
-;   (cljs.test/inc-report-counter! :fail)
-;   (println "\n  FAIL in" (cljs.test/testing-vars-str m))
-;   (when (seq (:testing-contexts (cljs.test/get-current-env)))
-;     (println (cljs.test/testing-contexts-str)))
-;   (when-let [message (:message m)]
-;     (println message))
-;   (println "    expected:" (pr-str (:expected m)))
-;   (println "      actual:" (pr-str (:actual m)))
-;   (println))
-
 (defn run_tests [callback]
-  (binding [test-callback callback]
-           (run-tests)))
+  (test/run-tests))
